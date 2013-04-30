19b1a5b34cda015d05b96ee1ada76d29aa89fd84
Gary Hockin <gary@hock.in>
2013-04-30 10:10:01 +0200

    Removed stupidly committed app id and secret

--- config/module.config.php
+++ config/module.config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'ZfcFacebook' => array(
-        'appid' => '448702521843756',
-        'appsecret' => '5f70dfcd4363faa46695e70f6b8b021b',
+        'appid' => '<appId>',
+        'appsecret' => '<appSecret>',
         'iframeapp' => true,
         'allowoutsidefacebook' => true,
         'notinstalledcontroller' => 'facebook',
